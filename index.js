03967f5365053675809e86f98d7ac561d11f98be
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2023-12-22 04:22:31 +0530

    Add structure

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 document.querySelector('footer').textContent = `© ${new Date().getFullYear()} malginisus`;
+function replaceClass() {
+    let main = document.getElementById('main');
+    let left = document.getElementById('left');
+    let right = document.getElementById('right');
+    let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth <= 516) {
+        main.classList.add('flex-col-rev');
+        left.classList.remove('flex-col');
+        right.classList.remove('flex-col');
+    } else {
+        main.classList.remove('flex-col-rev');
+        left.classList.add('flex-col');
+        right.classList.add('flex-col');
+    }
+}
+window.addEventListener('resize', replaceClass);
+replaceClass();
